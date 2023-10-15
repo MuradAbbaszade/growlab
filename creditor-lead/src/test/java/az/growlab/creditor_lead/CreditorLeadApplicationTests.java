f81e51cc6ff89212f38bba1263342fa653f95c99
unknown <muradabbaszade6@gmail.com>
2023-10-15 16:28:00 +0400

    Add creditor-lead app

---
+++ creditor-lead/src/test/java/az/growlab/creditor_lead/CreditorLeadApplicationTests.java
@@ -0,0 +1,13 @@
+package az.growlab.creditor_lead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditorLeadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
